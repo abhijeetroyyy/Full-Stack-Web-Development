b5d80976d15ad6d3efe81412a5ec594f990716a6
Abhijeet Roy <abhijeetrou123@gmail.com>
2024-02-19 22:25:20 +0530

    Initial commit

---
+++ Day 22/script.js
@@ -0,0 +1,15 @@
+console.log("Abhijeet");
+// let boxes=document.getElementsByClassName("box")
+// console.log(boxes)
+
+// boxes[2].style.backgroundColor = "red"
+
+// document.getElementById('red').style.backgroundColor = "red"
+
+// document.querySelector(".box").style.backgroundColor = "green"
+
+document.querySelectorAll(".box")
+
+document.querySelectorAll(".box").forEach(e =>{
+    e.style.backgroundColor = "green"
+})

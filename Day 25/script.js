22450b4308db564b6d5888cf40700d237c31a338
Abhijeet Roy <abhijeetrou123@gmail.com>
2024-02-22 23:21:36 +0530

    Initial commit

---
+++ Day 25/script.js
@@ -0,0 +1,21 @@
+let welcome_message = "Hello welcome to my website";
+console.log(welcome_message);
+let jeans_price = 15999 - ((10 / 10) * 15999 )- 272;
+let bag_price = 2499 - ((41 / 100) * 2499) - 29;
+let bag_value = Math.round(
+  ((jeans_price + bag_price - 29 + 25 + 20) * 118/ 100
+));
+let bag_sumarry = `your final  price is ₹${bag_value}`;
+console.log(bag_sumarry);
+
+let a=5;
+let b=6;
+let c= a+b;
+console.log(c);
+c=b+2;
+console.log(c);
+
+let i=5;
+console.log(i);
+i=i+5;
+console.log(i);
---
+++ Day 25/scripts.js
@@ -0,0 +1,2 @@
+let cartquantity=0;
+document.querySelector('#cardsumarry').innerHTML=`Your bag has ${cartquantity} items`;
